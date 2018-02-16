4d965edf9e234b33154c17577ddf2affdb977ea1
potpiejimmy <t.liese@outlook.com>
2018-02-16 23:10:03 +0100

    implementing gap circle trading

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const fetch = require('node-fetch');
 const WebSocket = require('ws');
 var fs = require("fs");
 
-const REF_ASSET = fs.readFileSync('REF_ASSET').toString();
-let lastAsset = REF_ASSET;
+let refAsset;  // reference asset to start trading with
 
 console.log("=== The Money Circulator 1.0.0 == " + new Date() + " ===\n");
 
@@ -14,28 +13,46 @@ if (!process.env.API_KEY || !process.env.API_SECRET) {
 }
 
 let prices = {};  // maps symbols to current prices (float)
+let bids = {}; // maps trade symbols to current bids prices (highest bid for selling)
 let assets = {};  // current assets
 let coins = [];   // known coins
-let circle = null;
+let circles = [];  // all circles, ordered by profitability
+let circleNo = 0;  // current trading circle no
 let valBefore = 0; // total REF_CUR value before trading
 let valAfter = 0; // total REF_CUR value after trading
 let tradeVolume = 0; // total theoretical trade volume (sum of all diff absolutes)
 let commission = 0; // total commission
 let listenKey; // web socket listen key
 let lastOrderResult;
-let tradeCount = 0;
+let tradeCount = 0; // number of trades traded in the current circle
 let lastTimeout;
 let cancellingTrade;
 
-// read all account assets
-readAssets()
+readPrices().then(allPrices => {
+    // add all prices to our map
+    allPrices.forEach(i => prices[i.symbol] = parseFloat(i.price));
+    // read all account assets
+    return readAssets();
+})
 .then(res => {
-    coins.push(REF_ASSET); // REF_ASSET first in list
+    // determine largest asset (refAsset):
+    let largestAsset = -1;
     res.balances.forEach(i => {
-        if (i.asset!=REF_ASSET) coins.push(i.asset);
         assets[i.asset]=parseFloat(i.free);
+        let valueUsdt = valueInUsdt(i.asset);
+        if (valueUsdt > largestAsset) {
+            largestAsset = valueUsdt;
+            refAsset = i.asset;
+        }
     });
-    console.log("Found " + coins.length + " assets. You have " + assets[REF_ASSET] + " " + REF_ASSET);
+    console.log("You have " + assets[refAsset] + " " + refAsset + " for trading.");
+
+    // build coin list for evaluation
+    coins.push(refAsset); // REF_ASSET first in list
+    res.balances.forEach(i => {
+        if (i.asset!=refAsset) coins.push(i.asset);
+    });
+    console.log("Building all tradeable coin circles for " + coins.length + " assets");
 
     // register listen key for websocket:
     return fetch("https://api.binance.com/api/v1/userDataStream", {method: 'POST', headers: {"X-MBX-APIKEY": process.env.API_KEY}});
@@ -49,7 +66,7 @@ readAssets()
     const ws = new WebSocket('wss://stream.binance.com:9443//ws/' + listenKey);
     ws.on('open', () => {
         console.log("WebSocket stream opened.");
-        startTrading();
+        calculateTrades();
     });
     ws.on('close', () => {
         console.log('WebSocket disconnected.');
@@ -59,27 +76,7 @@ readAssets()
         let e = JSON.parse(data);
         if (e.e == 'executionReport') {
             console.log(new Date() + " <<< " + data);
-            let qtyA = parseFloat(e.l);
-            let qtyB = qtyA * parseFloat(e.L);
-            if (e.S == 'SELL') {
-                assets[circle.c[tradeCount]] -= qtyA;
-                assets[circle.c[(tradeCount+1)%3]] += qtyB
-            } else {
-                assets[circle.c[tradeCount]] -= qtyB;
-                assets[circle.c[(tradeCount+1)%3]] += qtyA;
-            }
-            if (e.X == 'FILLED') {
-                // order fulfilled completely, trade next
-                clearTimeout(lastTimeout);
-                tradeCount++;
-                if (tradeCount == 3) {
-                    printCircleValue();
-                    console.log("SUCCESS.");
-                } else {
-                    lastAsset = circle.c[tradeCount];
-                    tradeNext();
-                }
-            }
+            handleExecutionReport(e);
         }
     });
 })
@@ -87,16 +84,26 @@ readAssets()
     console.log(err);
 });
 
-function startTrading() {
+function valueInUsdt(asset) {
+    let result = assets[asset];
+    if (asset == 'USDT') return result;
+    result *= prices['BTCUSDT'];
+    if (asset == 'BTC') return result; 
+    return result * prices[asset+'BTC'];
+}
+
+function readPrices() {
     // get all current prices from the public API
     let url = "https://api.binance.com/api/v1/ticker/allPrices";
     console.log("Reading prices: GET " + url);
-    fetch(url)
-    .then(res => res.json())
-    .then(allPrices => {
-        // add all prices to our map
+    return fetch(url).then(res => res.json());
+}
+
+function calculateTrades() {
+    // re-read current prices
+    return readPrices().then(allPrices => {
         allPrices.forEach(i => prices[i.symbol] = parseFloat(i.price));
-        let circles = [];
+        
         //for (let x=0; x<coins.length-2; x++) {
             let x=0; // build on REF_ASSET only
             for (let y=x+1; y<coins.length-1; y++) {
@@ -120,22 +127,86 @@ function startTrading() {
             console.log((i+1)+": "+c.c[0]+"->"+c.c[1]+"->"+c.c[2]+"->"+c.c[0]+" = " + c.p);
         }
 
-        circle = circles[0];
-        if (circle.p < 1) {
-            let swap = circle.c[1];
-            circle.c[1] = circle.c[2];
-            circle.c[2] = swap;
-        }
-        console.log("Okay, trading circle "+circle.c[0]+"->"+circle.c[1]+"->"+circle.c[2]+"->"+circle.c[0]+" to make "+circle.d.toFixed(2)+"% profit.");
+        circleNo = 0;
+        fetchBids();
 
-        return tradeNext();
+        // let circle = circles[circleNo];
+        // if (circle.p < 1) {
+        //     let swap = circle.c[1];
+        //     circle.c[1] = circle.c[2];
+        //     circle.c[2] = swap;
+        // }
+        // console.log("Okay, trading circle "+circle.c[0]+"->"+circle.c[1]+"->"+circle.c[2]+"->"+circle.c[0]+" to make "+circle.d.toFixed(2)+"% profit.");
+
+        // return tradeNext();
     });
 }
 
+function fetchBids() {
+    // fetch all bids and asks at once (open three requests if necessary)
+    if (!bids[circles[circleNo].c[0]+circles[circleNo].c[1]])
+        fetchBidsSingle(circles[circleNo].c[0], circles[circleNo].c[1]);
+
+    if (!bids[circles[circleNo].c[1]+circles[circleNo].c[2]])
+        fetchBidsSingle(circles[circleNo].c[1], circles[circleNo].c[2]);
+
+    if (!bids[circles[circleNo].c[2]+circles[circleNo].c[0]])
+        fetchBidsSingle(circles[circleNo].c[2], circles[circleNo].c[0]);
+}
+
+function fetchBidsSingle(a,b) {
+    let symbol;
+    if (prices[a+b]) {
+        symbol = a+b;
+    } else {
+        symbol = b+a;
+    }
+    let url = "https://api.binance.com/api/v1/depth?limit=5&symbol="+symbol;
+    console.log("Reading depth for "+symbol+": GET " + url);
+    return fetch(url).then(res => res.json()).then(depth => {
+        let bestBid = parseFloat(depth.bids[0][0]);
+        let bestAsk = parseFloat(depth.asks[0][0]);
+        bids[symbol] = (bestBid + bestAsk * 5) / 6;
+        if (symbol == a+b) {
+            bids[b+a] = 1/((bestAsk + bestBid * 5) / 6);
+        } else {
+            bids[a+b] = 1/((bestAsk + bestBid * 5) / 6);
+        }
+        if (isFetchBidsComplete()) evaluateCircle();
+    })
+    .catch(err => {
+        console.log(err);
+        process.exit(1);
+    });
+}
+
+function isFetchBidsComplete() {
+    return bids[circles[circleNo].c[0]+circles[circleNo].c[1]] &&
+           bids[circles[circleNo].c[1]+circles[circleNo].c[2]] &&
+           bids[circles[circleNo].c[2]+circles[circleNo].c[0]];
+}
+
+function evaluateCircle() {
+    let c = circles[circleNo];
+    let circleP = bids[c.c[0]+c.c[1]] * bids[c.c[1]+c.c[2]] * bids[c.c[2]+c.c[0]];
+    console.log("Evaluate Forwards:  "+c.c[0]+"->"+c.c[1]+"->"+c.c[2]+"->"+c.c[0]+" = " + circleP);
+    circleP = bids[c.c[0]+c.c[2]] * bids[c.c[2]+c.c[1]] * bids[c.c[1]+c.c[0]];
+    console.log("Evaluate Backwards: "+c.c[0]+"->"+c.c[2]+"->"+c.c[1]+"->"+c.c[0]+" = " + circleP);
+
+    circleNo++;
+    if (circleNo == circles.length) {
+        closeWebSocket().then(()=>{
+            process.exit(0);
+        });
+    } else {
+        fetchBids();
+    }
+}
+
 function tradeNext() {
-    printCircleValue(circle);
+    printCircleValue(circles[circleNo]);
     cancellingTrade = false; // opening a new trade
-    trade(circle.c[tradeCount],circle.c[(tradeCount+1)%3])
+    trade(circles[circleNo].c[tradeCount],circles[circleNo].c[(tradeCount+1)%3])
     .then(() => new Promise(resolve => lastTimeout = setTimeout(resolve, 60000)))
     .then(() => {
         console.log("Cancelling last orderId="+lastOrderResult.orderId);
@@ -152,8 +223,7 @@ function tradeNext() {
         if (!cancellingTrade) return response; // new trade opened, do nothing
         if (listenKey) response = response.then(()=>closeWebSocket());
         response.then(() => {
-            console.log("Done. New REF_ASSET = " + lastAsset + ".\n");
-            fs.writeFileSync("REF_ASSET", lastAsset);
+            console.log("Done.\n");
             process.exit(0);
         });
     })
@@ -161,6 +231,29 @@ function tradeNext() {
         console.log(err);
         process.exit(1);        
     });
+}
+
+function handleExecutionReport(e) {
+    let qtyA = parseFloat(e.l);
+    let qtyB = qtyA * parseFloat(e.L);
+    if (e.S == 'SELL') {
+        assets[circles[circleNo].c[tradeCount]] -= qtyA;
+        assets[circles[circleNo].c[(tradeCount+1)%3]] += qtyB
+    } else {
+        assets[circles[circleNo].c[tradeCount]] -= qtyB;
+        assets[circles[circleNo].c[(tradeCount+1)%3]] += qtyA;
+    }
+    if (e.X == 'FILLED') {
+        // order fulfilled completely, trade next
+        clearTimeout(lastTimeout);
+        tradeCount++;
+        if (tradeCount == 3) {
+            printCircleValue(circles[circleNo]);
+            console.log("SUCCESS.");
+        } else {
+            tradeNext();
+        }
+    }
 }
 
 function closeWebSocket() {
@@ -175,11 +268,11 @@ function printCircleValue(c) {
     let circleVal = 0;
     for (let i=0; i<3; i++) {
         let refVal = assets[c.c[i]];
-        if (c.c[i]!=REF_ASSET) refVal *= prices[c.c[i]+REF_ASSET] ? prices[c.c[i]+REF_ASSET] : 1/prices[REF_ASSET+c.c[i]];
-        console.log(assets[c.c[i]] + " " + c.c[i] + " = " + refVal + " " + REF_ASSET);
+        if (c.c[i]!=refAsset) refVal *= prices[c.c[i]+refAsset] ? prices[c.c[i]+refAsset] : 1/prices[refAsset+c.c[i]];
+        console.log(assets[c.c[i]] + " " + c.c[i] + " = " + refVal + " " + refAsset);
         circleVal += refVal;
     }
-    console.log("Total circle value: " + circleVal + " " + REF_ASSET);
+    console.log("Total circle value: " + circleVal + " " + refAsset);
 }
 
 function sign(params) {
@@ -225,7 +318,7 @@ function trade(a,b) {
     console.log(side + " " + quantity + " " + symbol + " for " + price + " (market: " + prices[symbol] + ")...");
 
     let params = "symbol=" + symbol + "&side=" + side + "&type=LIMIT&quantity=" + quantity + "&newOrderRespType=FULL&price="+price+"&timeInForce=GTC&timestamp=" + Date.now();
-    let url = "https://api.binance.com/api/v3/order?" + params + "&signature=" + sign(params);
+    let url = "https://api.binance.com/api/v3/order/test?" + params + "&signature=" + sign(params);
     return fetch(url, {method:'POST', headers: {"X-MBX-APIKEY": process.env.API_KEY}})
            .then(res => res.json())
            .then(res => {
